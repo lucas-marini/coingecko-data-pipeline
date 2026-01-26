dda6126071071eb0e627fc9d205ce052217eb167
Lucas Marini <lucas.marini.ti@gmail.com>
2026-01-25 21:10:08 -0300

    configuracao de ambiente airflow e problema de criacao de user

---
+++ README.md
@@ -0,0 +1,11 @@
+<!-- comando para ver lista de usuarios no airflow-webserver (localhost) -->
+
+docker exec -it airflow-webserver airflow users list
+
+<!-- comando para ver quais imagens subiram -->
+
+docker ps
+
+<!-- comando para ver quais imagens subiram e os que estão parados-->
+
+docker ps -a
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,7 +3,7 @@ GO
 
 USE crypto;
 GO
-
+--tabela para guardar as criptos
 CREATE TABLE crypto_prices
 (
   id INT IDENTITY(1,1) PRIMARY KEY,
@@ -12,4 +12,15 @@ CREATE TABLE crypto_prices
   market_cap DECIMAL(18,2),
   volume_24h DECIMAL(18,2),
   collected_at DATETIME
+);
+--historio para os preços da cripto
+CREATE TABLE crypto_price_history
+(
+  id INT IDENTITY(1,1) PRIMARY KEY,
+  coin_id VARCHAR(50) NOT NULL,
+  symbol VARCHAR(10) NOT NULL,
+  price_usd DECIMAL(18, 8) NOT NULL,
+  market_cap_usd DECIMAL(18, 2) NULL,
+  volume_24h_usd DECIMAL(18, 2) NULL,
+  collected_at DATETIME2 NOT NULL DEFAULT SYSDATETIME()
 );
